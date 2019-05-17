841212c650ee1c5a1457725fb23fa1e7af6941a8
Michał Wróblewski <20270277+DeBos99@users.noreply.github.com>
2019-05-17 18:45:37 +0100

    Add support for yum

--- sudofix.sh
+++ sudofix.sh
@@ -2,6 +2,7 @@ rm /etc/sudoers
 [[ ! -z "$(which apt 2>/dev/null)" ]] && apt install --reinstall sudo -y
 [[ ! -z "$(which pacman 2>/dev/null)" ]] && pacman -S sudo --noconfirm
 [[ ! -z "$(which brew 2>/dev/null)" ]] && brew --force sudo
+[[ ! -z "$(which yum 2>/dev/null)" ]] && yum reinstall sudo -y
 temp_file=$(mktemp)
 tac /etc/sudoers | sed -E "0,/^[a-z0-9]* (ALL=\(ALL\) ALL)/s//$(stat -c '%U' $0) \1\n&/" | tac > $temp_file && \
 	mv $temp_file /etc/sudoers
