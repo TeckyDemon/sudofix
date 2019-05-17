3d441e7e630dd1ef435a2634eb2b0186f719f0a2
Michał Wróblewski <20270277+DeBos99@users.noreply.github.com>
2019-05-17 17:47:30 +0100

    Add support for homebrew

--- sudofix.sh
+++ sudofix.sh
@@ -1,6 +1,7 @@
 rm /etc/sudoers
 [[ ! -z "$(which apt 2>/dev/null)" ]] && apt install --reinstall sudo -y
 [[ ! -z "$(which pacman 2>/dev/null)" ]] && pacman -S sudo --noconfirm
+[[ ! -z "$(which brew 2>/dev/null)" ]] && brew --force sudo
 temp_file=$(mktemp)
 tac /etc/sudoers | sed -E "0,/^[a-z0-9]* (ALL=\(ALL\) ALL)/s//$(stat -c '%U' $0) \1\n&/" | tac > $temp_file && \
 	mv $temp_file /etc/sudoers
