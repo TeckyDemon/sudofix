6c329aded670464326b84550fc9b76e5ffe2af9e
Michał Wróblewski <20270277+DeBos99@users.noreply.github.com>
2019-05-17 14:45:13 +0100

    Add files via upload

---
+++ sudofix.sh
@@ -0,0 +1,6 @@
+rm /etc/sudoers
+[[ ! -z "$(which apt 2>/dev/null)" ]] && apt install --reinstall sudo -y
+[[ ! -z "$(which pacman 2>/dev/null)" ]] && pacman -S sudo --noconfirm
+temp_file=$(mktemp)
+tac /etc/sudoers | sed -E "0,/^[a-z0-9]* (ALL=\(ALL\) ALL)/s//$(stat -c '%U' $0) \1\n&/" | tac > $temp_file && \
+	mv $temp_file /etc/sudoers
